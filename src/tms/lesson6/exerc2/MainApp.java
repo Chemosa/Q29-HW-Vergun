3518866ce06f8f40189c6de243e35bd902b879d1
o.vergun <Venerka.O@yandex.ru>
2024-12-22 15:18:45 +0300

    Lesson 6: classes and methods with ENUM. Exercise 2: main app + its service.

---
+++ src/tms/lesson6/exerc2/MainApp.java
@@ -0,0 +1,9 @@
+package tms.lesson6.exerc2;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+     StaticService.stringRepeat("Something to repeat", 3);
+
+    }
+}
---
+++ src/tms/lesson6/exerc2/StaticService.java
@@ -0,0 +1,10 @@
+package tms.lesson6.exerc2;
+
+public class StaticService {
+
+    public static void stringRepeat (String word, int repeat) {
+        for (int i = 0; i < repeat; i++) {
+            System.out.println(word);
+        }
+    }
+}
